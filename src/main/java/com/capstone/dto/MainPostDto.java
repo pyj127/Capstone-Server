887c53d282be3451c6618ae687d1667c0c50df22
kyunji <kyunji@ajou.ac.kr>
2022-04-26 20:22:50 +0900

    FIX : /order->/post, order_time 데이터 타입 수정

--- src/main/java/com/capstone/dto/MainPostDto.java
+++ src/main/java/com/capstone/dto/MainPostDto.java
@@ -3,14 +3,14 @@ package com.capstone.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
-import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Getter
 public class MainPostDto {
     private String title;
     private String rName;
     @JsonProperty("order_time")
-    private LocalDateTime orderTime;
+    private LocalTime orderTime;
     private String category;
     @JsonProperty("min_price")
     private int minPrice;
